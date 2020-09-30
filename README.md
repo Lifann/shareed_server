1f40b0d7382c0e19c93853e4024f2a52be071206
Lifann <xhlyfan@gmail.com>
2020-09-30 20:28:30 +0800

    Bugfix: uninitialized folders

--- README.md
+++ README.md
@@ -3,4 +3,3 @@
 A server management for https://github.com/Lifann/shareed_server.git
 with docker hub service for quick deployment.
 
-
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@
 sudo systemctl start docker
 
 # pull a docker image from docker hub
-docker pull oppenheimli/shareed_server:v1.0
+docker pull oppenheimli/shareed_server:v1.1
 
 # run the container
-docker run -d -it --network=host  --name shareed_server oppenheimli/shareed_server:v1.0 /bin/bash
+docker run -d -it --network=host  --name shareed_server oppenheimli/shareed_server:v1.1 /bin/bash
